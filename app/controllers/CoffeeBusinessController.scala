4d0bea68dde8fc18a376c78fbfb029c3b41e6c13
Richard Gibson <rich@9cy.net>
2017-05-14 21:45:37 +0100

    session 3 solutions

--- app/controllers/CoffeeBusinessController.scala
+++ app/controllers/CoffeeBusinessController.scala
@@ -3,10 +3,39 @@ package controllers
 import javax.inject.Inject
 
 import com.coffeeapp.repo._
+import play.api.Logger
 import play.api.mvc._
+
+import scala.util.{Failure, Success, Try}
 
 
 class CoffeeBusinessController @Inject()(businessRepository: BusinessRepository) extends Controller{
 
+  def listBusinesses = Action {
+    implicit request =>
+      Logger.debug("listBusinesses called")
+      val businesses = businessRepository.allBusinesses.blocking
+      Ok(views.html.admin.businesses(businesses))
+  }
+
+  def listBusinessById(id:Int) = Action {
+    implicit request =>
+      Logger.debug(s"listBusinesses called with id $id")
+      businessRepository.businessById(id).blocking match {
+        case Some(business) => Ok(views.html.admin.outlets(business))
+        case None => Ok(views.html.admin.msg("Business not found"))
+      }
+  }
+
+  def deleteBusinessById(id: Int) = Action {
+    implicit request =>
+      Logger.debug(s"deleteBusinessById called with id $id")
+      Try(businessRepository.deleteBusiness(id).blocking) match {
+        case Failure(_) => Ok(views.html.admin.msg("Error occurred deleting business"))
+        case Success(_) =>
+          Redirect(routes.CoffeeBusinessController.listBusinesses())
+      }
+
+  }
 
 }
