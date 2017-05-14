17e9064249495236814b179224cad2f4982db5c5
Richard Gibson <rich@9cy.net>
2017-05-14 20:53:07 +0100

    session 2 async solutions

--- app/controllers/CoffeeRestController.scala
+++ app/controllers/CoffeeRestController.scala
@@ -6,36 +6,43 @@ import com.coffeeapp.repo.{Business, BusinessRepository, Outlet, OutletRepositor
 import play.api.libs.json.{Json, OFormat}
 import play.api.mvc.{Action, Controller}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class CoffeeRestController @Inject()(businessRepository: BusinessRepository,
                                      outletRepository: OutletRepository) extends Controller {
 
+
   implicit val outletAsJson: OFormat[Outlet] = Json.format[Outlet]
   implicit val businessAsJson: OFormat[Business] = Json.format[Business]
 
-  def listByLocation(location: String) = Action {
-    val outlets = outletRepository.outletsByLocation(location).blocking
-    Ok(Json.toJson(outlets))
+  def listByLocation(location: String) = Action.async {
+    implicit request =>
+      outletRepository.outletsByLocation(location)
+        .map(outlets => Ok(Json.toJson(outlets)))
   }
 
-  def listByPostcode(postcode: String) = Action {
-    val outlets = outletRepository.outletsByPostcode(postcode).blocking
-    Ok(Json.toJson(outlets))
+  def listByPostcode(postcode: String) = Action.async {
+    implicit request =>
+      outletRepository.outletsByPostcode(postcode)
+        .map(outlets => Ok(Json.toJson(outlets)))
   }
 
-  def listAllOutlets = Action {
-    val outlets = outletRepository.allOutlets.blocking
-    Ok(Json.toJson(outlets))
+  def listAllOutlets = Action.async {
+    implicit request =>
+      outletRepository.allOutlets
+        .map(outlets => Ok(Json.toJson(outlets)))
   }
 
-  def listById(id: Int) = Action {
-    val business = businessRepository.businessById(id).blocking
-    Ok(Json.toJson(business))
+  def listById(id: Int) = Action.async {
+    implicit request =>
+      businessRepository.businessById(id)
+        .map(business => Ok(Json.toJson(business)))
   }
 
-  def listAllBusinesses = Action {
-    val business = businessRepository.allBusinesses.blocking
-    Ok(Json.toJson(business))
+  def listAllBusinesses = Action.async {
+    implicit request =>
+      businessRepository.allBusinesses
+        .map(business => Ok(Json.toJson(business)))
   }
 
 }
